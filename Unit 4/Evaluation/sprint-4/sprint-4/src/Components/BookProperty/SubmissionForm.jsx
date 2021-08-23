19355261a41212d4a8b7cb2179a9c05fb74607ba
Ramlala Yadav <easyexam01@gmail.com>
2021-08-23 15:42:43 +0530

    added modal

--- Unit 4/Evaluation/sprint-4/sprint-4/src/Components/BookProperty/SubmissionForm.jsx
+++ Unit 4/Evaluation/sprint-4/sprint-4/src/Components/BookProperty/SubmissionForm.jsx
@@ -3,12 +3,15 @@ import Button from '@material-ui/core/Button';
 import Modal from '@material-ui/core/Modal';
 import { useState } from 'react';
 import { Typography } from '@material-ui/core';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 
 export const SubmissionForm = () => {
 
     const [name, setName] = useState("")
     const [number, setNumber] = useState("")
+    const [sure, setSure] = useState(false)
     const handleSubmit = () => {
         console.log(name, number);
         setName("")
@@ -21,6 +24,9 @@ export const SubmissionForm = () => {
     const handleClose = () => {
 
     }
+    const handleOpen = () => {
+
+    }
 
     return <>
         <div>
@@ -30,21 +36,81 @@ export const SubmissionForm = () => {
                 Submit
             </Button>
 
-            <Modal
-
-                onClose={handleClose}
-                aria-labelledby="simple-modal-title"
-                aria-describedby="simple-modal-description"
-            >
-                <Typography>
-                    Open
-                </Typography>
-            </Modal>
-
+            <SimpleModal />
 
         </div>
 
     </>
+}
+
+
+
+
+function rand() {
+    return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
+export default function SimpleModal() {
+    const classes = useStyles();
+    // getModalStyle is not a pure function, we roll the style only on the first render
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(!open);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const body = (
+        <div style={modalStyle} className={classes.paper}>
+            <h2 id="simple-modal-title">  Are You Sure</h2>
+
+            <SimpleModal />
+        </div>
+    );
+
+    return (
+        <div>
+
+            <Button variant="contained" color="primary" onClick={handleOpen} >
+                Submit
+            </Button>
+
+            <Modal
+                open={open}
+                onClick={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                {body}
+            </Modal>
+        </div>
+    );
 }
 
 
