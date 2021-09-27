095c346d4691a98b56a791e76c911bd73fc89811
Ramlala Yadav <easyexam01@gmail.com>
2021-09-27 15:10:37 +0530

    added login register auth

--- Unit 5/Evaluation/Sprint 4/src/server.js
+++ Unit 5/Evaluation/Sprint 4/src/server.js
@@ -1,9 +1,32 @@
 
 const express = require("express")
 const connect = require("./configs/db")
+require("dotenv").config()
+
+const body = require("express-validator")
+const { register, login } = require("./controllers/auth.controller")
+const userController = require("./controllers/user.controller")
 
 
 const app = express()
+app.use(express.json());
+app.use('/users', userController)
+
+
+app.post("/signup",
+    body("name").isLength({ min: 1 }).withMessage("Please enter your name"),
+    body("password").isLength({ min: 5 }).withMessage("Please enter a valid password"),
+    body("email").isEmail().withMessage("Please enter valid email address"),
+    register
+)
+
+app.post("/signin",
+    body("name").isLength({ min: 1 }).withMessage("Please enter your name"),
+    body("password").isLength({ min: 5 }).withMessage("Please enter a valid password"),
+    body("email").isEmail().withMessage("Please enter valid email address"),
+    login
+)
+
 
 
 
